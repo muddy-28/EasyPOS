247cd808ccadd65e4d9a88ab9a665c647c352ed8
ss910423 <sasa.savic.upwork@gmail.com>
2019-08-23 05:18:44 +0200

    android build fix, onGestureHandlerStateChange issue

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler'
+import './thepatch';
 import './App/Config/ReactotronConfig'
 import { AppRegistry } from 'react-native'
 import App from './App/Containers/App'
